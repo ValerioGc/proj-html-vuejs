3c8284b2dffe8fd7522653b19209e6c555ce40ae
ValerioGc <valerio.gc1@gmail.com>
2022-09-05 16:50:57 +0200

    Avanzamento

---
+++ src/components/srcBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="srcBar">
+    <input v-bind="userInput" @change="$emit('usrIn', userInput)" type="text" id="search" placeholder="Search..." />
+    <i class="fa-solid fa-magnifying-glass"></i>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'srcBar',
+    data(){
+      return {
+        userInput: '',
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss">
+
+  @import "../styles/vars.scss";
+  @import "../styles/general.scss";
+
+  .srcBar {
+    position: relative;
+    flex-basis: 20%;
+
+    #search {
+      border-radius: 5px;
+      padding: 10px 8px;
+    }
+
+    .fa-magnifying-glass {
+      position: absolute;
+      font-weight: bold;
+      color: $material_jungle_green;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 6%;
+    }
+  }
+
+</style>
